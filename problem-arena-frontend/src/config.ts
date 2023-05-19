cf7020eea7089e9e73c097743d2acc6cf861efae
Andrei Cotor <andrei_cotor@yahoo.com>
2023-05-19 16:23:23 +0300

    [no-ticket] Made web socket secure

--- problem-arena-frontend/src/config.ts
+++ problem-arena-frontend/src/config.ts
@@ -2,4 +2,4 @@ import axios from "axios";
 
 export let axiosConfigured = axios.create({baseURL: "https://problemarena.mooo.com/api"});
 //export let axiosConfigured = axios.create({baseURL: "http://localhost:8000/api"});
-export let socketUrl = "ws://problemarena.mooo.com/api/chat/ws"
+export let socketUrl = "wss://problemarena.mooo.com/api/chat/ws"
