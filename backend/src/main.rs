f8ede35410bed46d9ed26ed1a45fe35595823120
Andrei Cotor <andrei_cotor@yahoo.com>
2023-05-19 15:44:33 +0300

    [no-ticket] Bug fix

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -64,6 +64,6 @@ async fn main() -> std::io::Result<()> {
                     .wrap(HttpAuthentication::bearer(admin_authentication_validator))
                     .configure(admin_config)
                 ))
-    }).bind(("127.0.0.1", 8000))?
+    }).bind(("0.0.0.0", 8000))?
         .run().await
 }
