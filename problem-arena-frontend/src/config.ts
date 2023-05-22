3d185999bd887f576fea95a20467101cc2802208
Andrei Cotor <andrei_cotor@yahoo.com>
2023-05-22 16:37:31 +0300

    [no-ticket] bug fix

--- problem-arena-frontend/src/config.ts
+++ problem-arena-frontend/src/config.ts
@@ -3,3 +3,4 @@ import axios from "axios";
 export let axiosConfigured = axios.create({baseURL: "https://problemarena.mooo.com/api"});
 //export let axiosConfigured = axios.create({baseURL: "http://localhost:8000/api"});
 export let socketUrl = "wss://problemarena.mooo.com/api/chat/ws"
+//export let socketUrl = "ws://localhost:8000/api/chat/ws"
